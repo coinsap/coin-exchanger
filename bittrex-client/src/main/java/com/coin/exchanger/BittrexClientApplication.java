769783273c193fc1120983f70271fc377fa2091d
semihbeceren <smhbeceren@gmail.com>
2017-12-13 22:48:30 +0300

    Add rest template bean

--- bittrex-client/src/main/java/com/coin/exchanger/BittrexClientApplication.java
+++ bittrex-client/src/main/java/com/coin/exchanger/BittrexClientApplication.java
@@ -2,7 +2,10 @@ package com.coin.exchanger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
@@ -12,4 +15,9 @@ public class BittrexClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BittrexClientApplication.class, args);
 	}
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplateBuilder().build();
+    }
 }
