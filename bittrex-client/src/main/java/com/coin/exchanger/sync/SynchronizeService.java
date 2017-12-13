302a37c4a70f4fe20c7a214e81f6e608e1ca5b46
Semih Beceren <smhbeceren@gmail.com>
2017-12-13 13:56:31 +0300

    Update SynchronizeService.java

--- bittrex-client/src/main/java/com/coin/exchanger/sync/SynchronizeService.java
+++ bittrex-client/src/main/java/com/coin/exchanger/sync/SynchronizeService.java
@@ -8,11 +8,11 @@ import com.coin.exchanger.remote.response.CurrencyHolder;
 import com.coin.exchanger.remote.response.MarketHolder;
 import com.coin.exchanger.remote.response.base.ResponseListWrapper;
 import com.coin.exchanger.remote.service.RemoteService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -22,6 +22,8 @@ import java.util.stream.Collectors;
  */
 @Service
 public class SynchronizeService {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     private final MarketRepository marketRepository;
     private final CurrencyRepository currencyRepository;
@@ -39,6 +41,9 @@ public class SynchronizeService {
         ResponseListWrapper<CurrencyHolder> currencyHolderResponseListWrapper = remoteService.getCurrenciesRestCall();
         Set<String> currencies = currencyRepository.findAllCurrencyNames();
         if (Objects.nonNull(currencyHolderResponseListWrapper) && currencyHolderResponseListWrapper.getSuccess() && Objects.nonNull(currencyHolderResponseListWrapper.getResult())) {
+            logger.info("Start Currency Sync");
+            logger.info("Currencies count on DB: {}", currencies.size());
+            logger.info("Currencies count on Request: {}", currencyHolderResponseListWrapper.getResult().size());
             currencyHolderResponseListWrapper.getResult()
                     .stream()
                     .filter(currencyHolder -> !currencies.contains(currencyHolder.getCurrency()))
@@ -46,6 +51,7 @@ public class SynchronizeService {
                     .stream()
                     .map(currencyHolder -> new Currency(currencyHolder.getCurrency(), currencyHolder.getTxFee(), currencyHolder.getActive(), currencyHolder.getCoinType()))
                     .forEach(currencyRepository::save);
+            logger.info("End Currency Sync");
         }
     }
 
@@ -53,13 +59,17 @@ public class SynchronizeService {
         ResponseListWrapper<MarketHolder> marketHolderResponseListWrapper = remoteService.getMarketsRestCall();
         Set<String> markets = marketRepository.findAllMarketNames();
         if (Objects.nonNull(marketHolderResponseListWrapper) && marketHolderResponseListWrapper.getSuccess() && Objects.nonNull(marketHolderResponseListWrapper.getResult())) {
+            logger.info("Start Market Sync");
+            logger.info("Markets count on DB: {}", markets.size());
+            logger.info("Markets count on Request: {}", marketHolderResponseListWrapper.getResult().size());
             marketHolderResponseListWrapper.getResult()
                     .stream()
                     .filter(marketHolder -> !markets.contains(marketHolder.getMarketName()))
                     .collect(Collectors.toList())
                     .stream()
-                    .map(marketHolder -> new Market(currencyRepository.findByCurrency(marketHolder.getMarketCurrency()), currencyRepository.findByCurrency(marketHolder.getBaseCurrency()), marketHolder.getMarketName(), marketHolder.getMinTradeSize(), marketHolder.getActive(), (Date) DateTimeFormatter.ISO_DATE_TIME.parse(marketHolder.getCreated())))
+                    .map(marketHolder -> new Market(currencyRepository.findByCurrency(marketHolder.getMarketCurrency()), currencyRepository.findByCurrency(marketHolder.getBaseCurrency()), marketHolder.getMarketName(), marketHolder.getMinTradeSize(), marketHolder.getActive(), marketHolder.getCreated()))
                     .forEach(marketRepository::save);
+            logger.info("End Market Sync");
         }
     }
 
