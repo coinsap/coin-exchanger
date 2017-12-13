c30e81423199fc5558f206dba8e5be92f1d839d8
Semih Beceren <smhbeceren@gmail.com>
2017-12-13 13:55:09 +0300

    Update RemoteService.java

--- bittrex-client/src/main/java/com/coin/exchanger/remote/service/RemoteService.java
+++ bittrex-client/src/main/java/com/coin/exchanger/remote/service/RemoteService.java
@@ -5,18 +5,11 @@ import com.coin.exchanger.remote.response.*;
 import com.coin.exchanger.remote.response.base.ResponseListWrapper;
 import com.coin.exchanger.remote.response.base.ResponseWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
-
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class RemoteService {
@@ -24,74 +17,61 @@ public class RemoteService {
     private final RestTemplate restTemplate;
     private static final String URI = "https://bittrex.com/api/v1.1/public/";
 
-
-
     @Autowired
-    public RemoteService(RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplate = restTemplateBuilder.build();
+    public RemoteService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
     public ResponseListWrapper<MarketHolder> getMarketsRestCall() {
-        return this.restTemplate.getForObject(URI + "getmarkets", (Class<ResponseListWrapper<MarketHolder>>) (Class<?>) ResponseListWrapper.class);
+        ParameterizedTypeReference<ResponseListWrapper<MarketHolder>> parameterizedTypeReference = new ParameterizedTypeReference<ResponseListWrapper<MarketHolder>>() {};
+        return this.restTemplate.exchange(URI + "getmarkets", HttpMethod.GET, null, parameterizedTypeReference).getBody();
     }
 
     public ResponseListWrapper<CurrencyHolder> getCurrenciesRestCall() {
-        ParameterizedTypeReference<ResponseListWrapper<CurrencyHolder>> parameterizedTypeReference = new ParameterizedTypeReference<ResponseListWrapper<CurrencyHolder>>() {
-            @Override
-            public Type getType() {
-                return super.getType();
-            }
-        };
-
+        ParameterizedTypeReference<ResponseListWrapper<CurrencyHolder>> parameterizedTypeReference = new ParameterizedTypeReference<ResponseListWrapper<CurrencyHolder>>() {};
         return this.restTemplate.exchange(URI + "getcurrencies", HttpMethod.GET, null, parameterizedTypeReference).getBody();
     }
 
     public ResponseWrapper<TickerHolder> getTickerRestCall(String market) {
+        ParameterizedTypeReference<ResponseWrapper<TickerHolder>> parameterizedTypeReference = new ParameterizedTypeReference<ResponseWrapper<TickerHolder>>() {};
         UriComponentsBuilder builder = UriComponentsBuilder
                 .fromUriString(URI+"getticker")
                 .queryParam("market", market);
-        return this.restTemplate.getForObject(builder.toUriString(), (Class<ResponseWrapper<TickerHolder>>) (Class<?>) ResponseWrapper.class);
+        return this.restTemplate.exchange(builder.toUriString(), HttpMethod.GET, null, parameterizedTypeReference).getBody();
     }
 
     public ResponseListWrapper<MarketSummaryHolder> getMarketSummariesRestCall() {
-        return this.restTemplate.getForObject(URI + "getmarketsummaries", (Class<ResponseListWrapper<MarketSummaryHolder>>) (Class<?>) ResponseListWrapper.class);
+        ParameterizedTypeReference<ResponseListWrapper<MarketSummaryHolder>> parameterizedTypeReference = new ParameterizedTypeReference<ResponseListWrapper<MarketSummaryHolder>>() {};
+        return this.restTemplate.exchange(URI + "getmarketsummaries", HttpMethod.GET, null, parameterizedTypeReference).getBody();
     }
 
     public ResponseWrapper<MarketSummaryHolder> getMarketSummaryRestCall(String market) {
+        ParameterizedTypeReference<ResponseListWrapper<MarketSummaryHolder>> parameterizedTypeReference = new ParameterizedTypeReference<ResponseListWrapper<MarketSummaryHolder>>() {};
         UriComponentsBuilder builder = UriComponentsBuilder
                 .fromUriString(URI+"getmarketsummary")
                 .queryParam("market", market);
-        return this.restTemplate.getForObject(builder.toUriString(), (Class<ResponseWrapper<MarketSummaryHolder>>) (Class<?>) ResponseWrapper.class);
-
+        ResponseListWrapper<MarketSummaryHolder> marketSummaryHolderResponseListWrapper = this.restTemplate.exchange(builder.toUriString(), HttpMethod.GET, null, parameterizedTypeReference).getBody();
+        ResponseWrapper<MarketSummaryHolder> marketSummaryHolderResponseWrapper = new ResponseWrapper<>();
+        marketSummaryHolderResponseWrapper.setResult(marketSummaryHolderResponseListWrapper.getResult().get(0));
+        marketSummaryHolderResponseWrapper.setSuccess(marketSummaryHolderResponseListWrapper.getSuccess());
+        marketSummaryHolderResponseWrapper.setMessage(marketSummaryHolderResponseListWrapper.getMessage());
+        return marketSummaryHolderResponseWrapper;
     }
 
     public ResponseWrapper<OrderBookHolder> getOrderBookRestCall(String market) {
+        ParameterizedTypeReference<ResponseWrapper<OrderBookHolder>> parameterizedTypeReference = new ParameterizedTypeReference<ResponseWrapper<OrderBookHolder>>() {};
         UriComponentsBuilder builder = UriComponentsBuilder
                 .fromUriString(URI+"getorderbook")
                 .queryParam("market", market)
                 .queryParam("type", OrderType.BOTH.name().toLowerCase());
-
-        ResponseWrapper<LinkedHashMap> orders = this.restTemplate.getForObject(builder.toUriString(), (Class<ResponseWrapper<LinkedHashMap>>) (Class<?>) ResponseWrapper.class);
-        return getOrderListWrapper(orders);
-    }
-
-    private ResponseWrapper<OrderBookHolder> getOrderListWrapper(ResponseWrapper<LinkedHashMap> orders) {
-        List<LinkedHashMap<String, Double>> buy = (List<LinkedHashMap<String, Double>>) orders.getResult().get(OrderType.BUY.name().toLowerCase());
-        List<LinkedHashMap<String, Double>> sell = (List<LinkedHashMap<String, Double>>) orders.getResult().get(OrderType.SELL.name().toLowerCase());
-        ResponseWrapper<OrderBookHolder> responseWrapper = new ResponseWrapper<>();
-        OrderBookHolder orderBookHolder = new OrderBookHolder();
-        orderBookHolder.setBuy(buy.stream().map(a -> new OrderHolder(a.get("Quantity"), a.get("Rate"))).collect(Collectors.toCollection(ArrayList::new)));
-        orderBookHolder.setSell(sell.stream().map(a -> new OrderHolder(a.get("Quantity"), a.get("Rate"))).collect(Collectors.toCollection(ArrayList::new)));
-        responseWrapper.setMessage(orders.getMessage());
-        responseWrapper.setSuccess(orders.getSuccess());
-        responseWrapper.setResult(orderBookHolder);
-        return responseWrapper;
+        return this.restTemplate.exchange(builder.toUriString(), HttpMethod.GET, null, parameterizedTypeReference).getBody();
     }
 
     public ResponseListWrapper<MarketHistoryHolder> getMarketHistoryRestCall(String market) {
+        ParameterizedTypeReference<ResponseListWrapper<MarketHistoryHolder>> parameterizedTypeReference = new ParameterizedTypeReference<ResponseListWrapper<MarketHistoryHolder>>() {};
         UriComponentsBuilder builder = UriComponentsBuilder
                 .fromUriString(URI+"getmarkethistory")
                 .queryParam("market", market);
-        return this.restTemplate.<ResponseListWrapper<MarketHistoryHolder>>getForObject(builder.toUriString(), (Class<ResponseListWrapper<MarketHistoryHolder>>) (Class<?>) ResponseListWrapper.class);
+        return this.restTemplate.exchange(builder.toUriString(), HttpMethod.GET, null, parameterizedTypeReference).getBody();
     }
 }
