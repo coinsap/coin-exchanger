08b754e3d45ce0ae38ad0665a9e1b229ba0f7cfc
Semih Beceren <smhbeceren@gmail.com>
2017-12-15 14:40:23 +0300

    Update MarketHistoryRepository.java

--- bittrex-client/src/main/java/com/coin/exchanger/market/history/MarketHistoryRepository.java
+++ bittrex-client/src/main/java/com/coin/exchanger/market/history/MarketHistoryRepository.java
@@ -1,7 +1,11 @@
 package com.coin.exchanger.market.history;
 
+import com.coin.exchanger.market.Market;
+import com.coin.exchanger.remote.response.OrderType;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * @author Semih Beceren
@@ -9,4 +13,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MarketHistoryRepository extends CrudRepository<MarketHistory, Long> {
     MarketHistory findByApiId(Long apiId);
+
+    List<MarketHistory> findTop10ByMarketAndOrderTypeOrderByTimestampDesc(Market market, OrderType orderType);
 }
