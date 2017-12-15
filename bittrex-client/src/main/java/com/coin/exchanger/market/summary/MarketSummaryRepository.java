75aae23421297bdf255cd1999a6f8819be02e345
Semih Beceren <smhbeceren@gmail.com>
2017-12-15 14:41:14 +0300

    Update MarketSummaryRepository.java

--- bittrex-client/src/main/java/com/coin/exchanger/market/summary/MarketSummaryRepository.java
+++ bittrex-client/src/main/java/com/coin/exchanger/market/summary/MarketSummaryRepository.java
@@ -3,10 +3,13 @@ package com.coin.exchanger.market.summary;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * @author Semih Beceren
  */
 @Repository
 public interface MarketSummaryRepository extends CrudRepository<MarketSummary, Long> {
+    List<MarketSummary> findAll();
     MarketSummary findByHash(String hash);
 }
